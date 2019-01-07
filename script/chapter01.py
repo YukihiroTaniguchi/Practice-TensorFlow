3f55c6683049f1d64bf0e3d6f243f762a78d3c77
y.taniguchi <y.softvalley@gmail.com>
2019-01-07 22:41:35 +0900

    データソースを操作する

--- script/chapter01.py
+++ script/chapter01.py
@@ -136,3 +136,30 @@ print(sess.run(tf.nn.tanh([-1., 0., 1.])))
 print(sess.run(tf.nn.softsign([-1., 0., 1.])))
 
 print(sess.run(tf.nn.softplus([-1., 0., 1.])))
+
+print(sess.run(tf.nn.elu([-1., 0., 1.])))
+
+ # 1.8 データソースを操作する
+
+ # Iris データセット
+from sklearn import datasets
+iris = datasets.load_iris()
+print(len(iris.data))
+print(len(iris.target))
+print(iris.target[0])
+print(set(iris.target))
+
+# Low Birthweightデータセット
+import requests
+
+birthdata_url = 'https://github.com/nfmcclure/tensorflow_cookbook/raw' \
+    'master/01_Introduction/07_Working_with_Data_Sources/' \
+    'birthweight_data/birthweight.dat'
+
+birth_file = requests.get(birthdata_url)
+birth_data = birth_file.text.split('\r\n')
+birth_header = birth_data[0].split('\t')
+birth_data = [[float(x) for x in y.split('\t') if len(x)>=1]
+    for y in birth_data[1:] if len(y)>=1]
+
+print(len(birth_data))
