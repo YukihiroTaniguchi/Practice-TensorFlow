1792becc9c45924f7728b81a3e7ba122abca6820
y.taniguchi <y.softvalley@gmail.com>
2019-01-11 08:33:18 +0900

    ロジスティック回帰の実装

--- script/chapter03.py
+++ script/chapter03.py
@@ -489,7 +489,7 @@ test_indices = np.array(list(set(range(len(x_vals))) - set(train_indices)))
 x_vals_train = x_vals[train_indices]
 x_vals_test = x_vals[test_indices]
 y_vals_train = y_vals[train_indices]
-y_vals_tet = y_vals[test_indices]
+y_vals_test = y_vals[test_indices]
 
 # 列で正規化
 def normalize_cols(m):
@@ -499,3 +499,60 @@ def normalize_cols(m):
 
 x_vals_train = np.nan_to_num(normalize_cols(x_vals_train))
 x_vals_train = np.nan_to_num(normalize_cols(x_vals_train))
+
+# バチサイズの設定、プレースホルダの作成、変数とロジスティックモデルの定義
+batch_size = 25
+x_data = tf.placeholder(shape=[None, 7], dtype=tf.float32)
+y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+A = tf.Variable(tf.random_normal(shape=[7, 1]))
+b = tf.Variable(tf.random_normal(shape=[1, 1]))
+model_output = tf.add(tf.matmul(x_data, A), b)
+
+# 損失関数を指定
+loss =tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+    logits=model_output, labels=y_target))
+# 変数を初期化
+init = tf.global_variables_initializer()
+sess.run(init)
+
+# 最適化関数を指定
+my_opt = tf.train.GradientDescentOptimizer(0.01)
+train_step = my_opt.minimize(loss)
+
+# 実際の予測
+prediction = tf.round(tf.sigmoid(model_output))
+predictions_correct = tf.cast(tf.equal(prediction, y_target), tf.float32)
+accuracy = tf.reduce_mean(predictions_correct)
+
+loss_vec = []
+train_acc = []
+test_acc = []
+for i in range(1500):
+    rand_index = np.random.choice(len(x_vals_train), size=batch_size)
+    rand_x = x_vals_train[rand_index]
+    rand_y = np.transpose([y_vals_train[rand_index]])
+    sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
+
+    temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
+    loss_vec.append(temp_loss)
+    temp_acc_train = sess.run(accuracy, feed_dict={x_data: x_vals_train, \
+                                       y_target: np.transpose([y_vals_train])})
+    train_acc.append(temp_acc_train)
+    temp_acc_test = sess.run(accuracy, feed_dict={x_data: x_vals_test, \
+                                       y_target: np.transpose([y_vals_test])})
+    test_acc.append(temp_acc_test)
+
+plt.plot(loss_vec, 'k-')
+plt.title('Cross Entropy Loss per Generation')
+plt.xlabel('Generation')
+plt.ylabel('Cross Entropy Loss')
+plt.show()
+
+plt.plot(train_acc, 'k-', label='Train Set Accuracy')
+plt.plot(test_acc, 'r--', label='Test Set Accuracy')
+plt.title('Train and Test Accuracy')
+plt.xlabel('Generation')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.show()
+    
