60a00cadec9ae20276a4651105d125f9edc07ae1
y.taniguchi <y.softvalley@gmail.com>
2019-01-12 00:32:25 +0900

    SVMを線形回帰へ

--- script/chapter04.py
+++ script/chapter04.py
@@ -88,22 +88,149 @@ for i in range(500):
         print(f'Step #{str(i+1)} A = {str(sess.run(A))}, b = {str(sess.run(b))}')
         print('Loss =', str(temp_loss))
 
-    # 係数を抽出
-    [[a1], [a2]] = sess.run(A)
-    [[b]] = sess.run(b)
-    slope = -a2/a1
-    y_intercept = b/a1
+# 係数を抽出
+[[a1], [a2]] = sess.run(A)
+[[b]] = sess.run(b)
+slope = -a2/a1
+y_intercept = b/a1
+x1_vals = [d[1] for d in x_vals]
 
-    x1_vals = [d[1] for d in x_vals]
+# 最も適合する直線を取得
+best_fit = []
+for i in x1_vals:
+    best_fit.append(slope*i+y_intercept)
 
-    # 最も適合する直線を取得
-    best_fit = []
-    for i in x1_vals:
-        best_fit.append(slope*i+y_intercept)
+# Setosa かどうかで分割
+setosa_x = [d[1] for i,d in enumerate(x_vals) if y_vals[i] == [1]]
+setosa_y = [d[0] for i,d in enumerate(x_vals) if y_vals[i] == [1]]
+not_setosa_x = [d[1] for i,d in enumerate(x_vals) if y_vals[i] == -1]
+not_setosa_y = [d[0] for i,d in enumerate(x_vals) if y_vals[i] == -1]
 
-    # Setosa かどうかで分割
-    setosa_x = [d[1] for i,d in enumerate(x_vals) if y_vals[i] == [1]]
-    setosa_y = [d[0] for i,d in enumerate(x_vals) if y_vals[i] == [1]]
+# データと最も適合する直線をプロット
+plt.plot(setosa_x, setosa_y, 'o', label='I. setosa')
+plt.plot(not_setosa_x, not_setosa_y, 'x', label='Non-setosa')
+plt.plot(x1_vals, best_fit, 'r-', label='Linear Separator', linewidth=3)
+plt.ylim([0, 10])
+plt.legend(loc='lower right')
+plt.title('Sepal Length vs Pedal Width')
+plt.xlabel('Pedal Width')
+plt.ylabel('Sepal Length')
+plt.show()
 
-    not_setosa_x = [d[1] for i,d in enumerate(x_vals) if y_vals[i] == -1]
-    not_setosa_x = [d[0] for i,d in enumerate(x_vals) if y_vals[i] == -1]
+plt.plot(train_accuracy, 'k-', label='Training Accuracy')
+plt.plot(test_accuracy, 'r--', label='Test Accuracy')
+plt.title('Train and Test Set Accuracies')
+plt.xlabel('Generation')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.show()
+
+# 損失地をプロット
+plt.plot(loss_vec, 'k-')
+plt.title('Loss per Generation')
+plt.xlabel('Generation')
+plt.ylabel('Loss')
+plt.show()
+
+# 線形回帰への縮約
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from sklearn import datasets
+
+sess = tf.Session()
+
+iris = datasets.load_iris()
+# iris.data = [(がく片の長さ、がく片の幅、花びらの長さ、花びらの幅)]
+x_vals = np.array([x[3] for x in iris.data])
+y_vals = np.array([y[0] for y in iris.data])
+
+# データセットをトレーニングセットとテストセットに分割
+train_indices = np.random.choice(len(x_vals), round(len(x_vals)*0.8),\
+                                 replace=False)
+test_indices = np.array(list(set(range(len(x_vals))) - set(train_indices)))
+x_vals_train = x_vals[train_indices]
+x_vals_test = x_vals[test_indices]
+y_vals_train = y_vals[train_indices]
+y_vals_test = y_vals[test_indices]
+
+batch_size = 50
+
+x_data = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+
+A = tf.Variable(tf.random_normal(shape=[1, 1]))
+b = tf.Variable(tf.random_normal(shape=[1, 1]))
+
+model_output = tf.add(tf.matmul(x_data, A), b)
+
+# 損失関数を指定
+epsilon = tf.constant([0.5])
+loss = tf.reduce_mean(tf.maximum(0., tf.subtract(tf.abs( \
+                      tf.subtract(model_output, y_target)), epsilon)))
+
+my_opt = tf.train.GradientDescentOptimizer(0.075)
+train_step = my_opt.minimize(loss)
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+train_loss = []
+test_loss = []
+for i in range(200):
+    rand_index = np.random.choice(len(x_vals_train), size=batch_size)
+    rand_x = np.transpose([x_vals_train[rand_index]])
+    rand_y = np.transpose([y_vals_train[rand_index]])
+    sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
+
+    temp_train_loss = sess.run(loss, feed_dict={
+        x_data: np.transpose([x_vals_train]),
+        y_target: np.transpose([y_vals_train])})
+    train_loss.append(temp_train_loss)
+
+    temp_test_loss = sess.run(loss, feed_dict={
+        x_data: np.transpose([x_vals_test]),
+        y_target: np.transpose([y_vals_test])})
+    test_loss.append(temp_test_loss)
+
+    if (i+1)%50==0:
+        print('------------')
+        print('Generation:', str(i))
+        print(f'A = {str(sess.run(A))} b = f{str(sess.run(b))}')
+        print('Train Loss =', str(temp_train_loss))
+        print('Test Loss =', str(temp_test_loss))
+
+# 係数を抽出
+[[slope]] = sess.run(A)
+[[y_intercept]] = sess.run(b)
+[width] = sess.run(epsilon)
+
+# 最も適合する直線を取得
+best_fit = []
+best_fit_upper = []
+best_fit_lower = []
+for i in x_vals:
+    best_fit.append(slope*i+y_intercept)
+    best_fit_upper.append(slope*i+y_intercept+width)
+    best_fit_lower.append(slope*i+y_intercept-width)
+
+# 最も適合する直線をプロット
+plt.plot(x_vals, y_vals, 'o', label='Data Points')
+plt.plot(x_vals, best_fit, 'r-', label='SVM Regression Lin', linewidth=3)
+plt.plot(x_vals, best_fit_upper, 'r--', linewidth=2)
+plt.plot(x_vals, best_fit_lower, 'r--', linewidth=2)
+plt.ylim([0, 10])
+plt.legend(loc='lower right')
+plt.title('Sepal Length vs Pedal Width')
+plt.xlabel('Pedal Width')
+plt.ylabel('Sepal Length')
+plt.show()
+
+# 損失値をプロット
+plt.plot(train_loss, 'k-', label='Train Set Loss')
+plt.plot(test_loss, 'r--', label='Test Set Loss')
+plt.title('L2 Loss per Generation')
+plt.xlabel('Generation')
+plt.ylabel('L2 Loss')
+plt.legend(loc='upper right')
+plt.show()
