a35d31b5299710936783481e21e8bd41878198ca
y.taniguchi <y.softvalley@gmail.com>
2019-01-11 09:52:25 +0900

    SVMの実装2

--- script/chapter04.py
+++ script/chapter04.py
@@ -47,3 +47,43 @@ classification_term = \
 
 # 損失関数の第1項と第2項を加算
 loss = tf.add(classification_term, tf.multiply(alpha, l2_norm))
+
+# 予測関数を指定
+prediction = tf.sign(model_output)
+# 正解率を算出する正解関数を指定
+accuracy = tf.reduce_mean(tf.cast(tf.equal(prediction, y_target), \
+                                  tf.float32))
+
+# 最適化関数を作成し、モデルの変数を初期化する
+my_opt = tf.train.GradientDescentOptimizer(0.01)
+train_step = my_opt.minimize(loss)
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+# 勾配、切片、損失値などを求める処理を繰り返すトレーニングループ
+loss_vec = []
+train_accuracy = []
+test_accuracy = []
+for i in range(500):
+    rand_index = np.random.choice(len(x_vals_train), size=batch_size)
+    rand_x = x_vals_train[rand_index]
+    rand_y = np.transpose([y_vals[rand_index]])
+    sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
+
+    temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
+    loss_vec.append(temp_loss)
+
+    train_acc_temp = sess.run(accuracy, feed_dict={
+        x_data: x_vals_train,
+        y_target: np.transpose([y_vals_train])})
+    train_accuracy.append(train_acc_temp)
+
+    test_acc_temp = sess.run(accuracy, feed_dict={
+        x_data: x_vals_test,
+        y_target: np.transpose([y_vals_test])})
+    test_accuracy.append(test_acc_temp)
+
+    if (i+1)%100==0:
+        print(f'Step #{str(i+1)} A = {str(sess.run(A))}, b = {str(sess.run(b))}')
+        print('Loss =', str(temp_loss))
